0351f3c51c732da1bd4e2b6e6976c3bf503d8b3c
Xavier Lau <c.estlavie@icloud.com>
2021-06-10 09:14:32 +0800

    companion for #cumulus-472

--- runtime/src/pallets/balances.rs
+++ runtime/src/pallets/balances.rs
@@ -11,14 +11,17 @@ pub type NegativeImbalance =
 frame_support::parameter_types! {
 	pub const ExistentialDeposit: Balance = 0;
 	pub const MaxLocks: u32 = 50;
+	pub const MaxReserves: u32 = 50;
 }
 
 impl Config for Runtime {
-	type MaxLocks = MaxLocks;
 	type Balance = Balance;
 	type Event = Event;
 	type DustRemoval = ();
 	type ExistentialDeposit = ExistentialDeposit;
 	type AccountStore = System;
+	type MaxLocks = MaxLocks;
+	type MaxReserves = MaxReserves;
+	type ReserveIdentifier = [u8; 8];
 	type WeightInfo = WeightInfo<Runtime>;
 }
