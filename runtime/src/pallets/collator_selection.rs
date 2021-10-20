b363ed0af0c5afa15c9e6ae5ee42816d672a11ec
Xavier Lau <hi@xavier.zone>
2021-10-20 15:45:25 +0800

    Companion for #Cumulus-498

--- runtime/src/pallets/collator_selection.rs
+++ runtime/src/pallets/collator_selection.rs
@@ -11,6 +11,7 @@ frame_support::parameter_types! {
 	pub const ExecutiveBody: BodyId = BodyId::Executive;
 	pub const PotId: PalletId = PalletId(*b"PotStake");
 	pub const MaxCandidates: u32 = 1000;
+	pub const MinCandidates: u32 = 5;
 	pub const MaxInvulnerables: u32 = 100;
 }
 
@@ -27,8 +28,12 @@ impl Config for Runtime {
 	type UpdateOrigin = CollatorSelectionUpdateOrigin;
 	type PotId = PotId;
 	type MaxCandidates = MaxCandidates;
+	type MinCandidates = MinCandidates;
 	type MaxInvulnerables = MaxInvulnerables;
 	// should be a multiple of session or things will get inconsistent
 	type KickThreshold = Period;
+	type ValidatorId = <Self as frame_system::Config>::AccountId;
+	type ValidatorIdOf = pallet_collator_selection::IdentityCollator;
+	type ValidatorRegistration = Session;
 	type WeightInfo = WeightInfo<Runtime>;
 }
