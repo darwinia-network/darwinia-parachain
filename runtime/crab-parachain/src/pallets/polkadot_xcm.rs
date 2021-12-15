5069e2f61ed27c3d50b805e8f4a6af9f0c59ee95
Xavier Lau <xavier@inv.cafe>
2021-12-16 06:25:10 +0800

    Companion for Cumulus#690

--- runtime/crab-parachain/src/pallets/polkadot_xcm.rs
+++ runtime/crab-parachain/src/pallets/polkadot_xcm.rs
@@ -67,10 +67,10 @@ pub type XcmOriginToTransactDispatchOrigin = (
 	// foreign chains who want to have a local sovereign account on this chain which they control.
 	SovereignSignedViaLocation<LocationToAccountId, Origin>,
 	// Native converter for Relay-chain (Parent) location; will converts to a `Relay` origin when
-	// recognised.
+	// recognized.
 	RelayChainAsNative<RelayChainOrigin, Origin>,
 	// Native converter for sibling Parachains; will convert to a `SiblingPara` origin when
-	// recognised.
+	// recognized.
 	SiblingParachainAsNative<CumulusOrigin, Origin>,
 	// Superuser converter for the Relay-chain (Parent) location. This will allow it to issue a
 	// transaction from the Root origin.
