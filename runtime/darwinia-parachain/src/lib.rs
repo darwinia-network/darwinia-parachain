f2ddd2ac902cf451e14bcf96993cbee6afe3d420
Xavier Lau <xavier@inv.cafe>
2022-02-15 17:51:24 +0800

    Companion for Cumulus#86f76c5619c64d1300315612695ad4b4fcd0f562

--- runtime/darwinia-parachain/src/lib.rs
+++ runtime/darwinia-parachain/src/lib.rs
@@ -255,7 +255,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
-	state_version: 1,
+	state_version: 0,
 };
 
 /// The version information used to identify this runtime when compiled natively.
