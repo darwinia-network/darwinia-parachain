a73dbccfc8d9b6603db8307f40ff556ab5729290
Xavier Lau <xavier@inv.cafe>
2022-02-15 17:03:51 +0800

    Companion for Cumulus#678

--- src/command.rs
+++ src/command.rs
@@ -233,16 +233,15 @@ pub fn run() -> Result<()> {
 						.iter()
 						.chain(cli.relay_chain_args.iter()),
 				);
-
 				let id = ParaId::from(para_id);
-
 				let parachain_account =
 					AccountIdConversion::<polkadot_primitives::v0::AccountId>::into_account(&id);
-
-				let block: OpaqueBlock =
-					generate_genesis_block(&config.chain_spec).map_err(|e| format!("{:?}", e))?;
+				let state_version =
+					RelayChainCli::native_runtime_version(&config.chain_spec).state_version();
+				let block: crate::service::Block =
+					generate_genesis_block(&config.chain_spec, state_version)
+						.map_err(|e| format!("{:?}", e))?;
 				let genesis_state = format!("0x{:?}", HexDisplay::from(&block.header().encode()));
-
 				let tokio_handle = config.tokio_handle.clone();
 				let polkadot_config =
 					SubstrateCli::create_configuration(&polkadot_cli, &polkadot_cli, tokio_handle)
@@ -323,11 +322,13 @@ pub fn run() -> Result<()> {
 		}),
 		Some(Subcommand::ExportGenesisState(params)) => {
 			let mut builder = sc_cli::LoggerBuilder::new("");
-			builder.with_profiling(sc_tracing::TracingReceiver::Log, "");
-			let _ = builder.init();
 
-			let block: OpaqueBlock =
-				generate_genesis_block(&load_spec(&params.chain.clone().unwrap_or_default())?)?;
+			builder.with_profiling(sc_tracing::TracingReceiver::Log, "");
+
+			let _ = builder.init();
+			let spec = load_spec(&params.chain.clone().unwrap_or_default())?;
+			let state_version = Cli::native_runtime_version(&spec).state_version();
+			let block: crate::service::Block = generate_genesis_block(&spec, state_version)?;
 			let raw_header = block.header().encode();
 			let output_buf = if params.raw {
 				raw_header
