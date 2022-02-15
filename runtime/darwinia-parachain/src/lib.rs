d65010d7f99db1ddc943790af9206b8dd524b164
Xavier Lau <xavier@inv.cafe>
2022-02-15 14:38:09 +0800

    Companion for Cumulus#847

--- runtime/darwinia-parachain/src/lib.rs
+++ runtime/darwinia-parachain/src/lib.rs
@@ -217,6 +217,7 @@ pub type Block = generic::Block<Header, UncheckedExtrinsic>;
 pub type SignedBlock = generic::SignedBlock<Block>;
 /// The SignedExtension to the basic transaction logic.
 pub type SignedExtra = (
+	frame_system::CheckNonZeroSender<Runtime>,
 	frame_system::CheckSpecVersion<Runtime>,
 	frame_system::CheckTxVersion<Runtime>,
 	frame_system::CheckGenesis<Runtime>,
