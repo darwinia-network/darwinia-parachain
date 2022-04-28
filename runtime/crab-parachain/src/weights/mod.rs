e3f09f575dd5931df791151f07e74217789b34e6
Guantong <04637@163.com>
2022-04-28 14:36:53 +0800

    add benchmark and update weight for crab-parachain

---
+++ runtime/crab-parachain/src/weights/mod.rs
@@ -0,0 +1,7 @@
+pub mod frame_system;
+pub mod pallet_balances;
+pub mod pallet_collator_selection;
+pub mod pallet_multisig;
+pub mod pallet_proxy;
+pub mod pallet_timestamp;
+pub mod pallet_utility;
