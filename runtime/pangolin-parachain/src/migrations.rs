1ff72926bb9f8d24862cebf945fdf8ca09d8e8fa
Guantong <04637@163.com>
2022-05-18 16:25:08 +0800

    merge #72

---
+++ runtime/pangolin-parachain/src/migrations.rs
@@ -0,0 +1,30 @@
+// --- paritytech ---
+#[allow(unused)]
+use frame_support::{migration, traits::OnRuntimeUpgrade, weights::Weight};
+// --- darwinia-network ---
+#[allow(unused)]
+use crate::*;
+
+pub struct CustomOnRuntimeUpgrade;
+impl OnRuntimeUpgrade for CustomOnRuntimeUpgrade {
+	#[cfg(feature = "try-runtime")]
+	fn pre_upgrade() -> Result<(), &'static str> {
+		Ok(())
+	}
+
+	#[cfg(feature = "try-runtime")]
+	fn post_upgrade() -> Result<(), &'static str> {
+		Ok(())
+	}
+
+	fn on_runtime_upgrade() -> Weight {
+		migrate()
+	}
+}
+
+fn migrate() -> Weight {
+	migration::move_pallet(b"FeeMarket", b"PangolinFeeMarket");
+
+	// 0
+	RuntimeBlockWeights::get().max_block
+}
