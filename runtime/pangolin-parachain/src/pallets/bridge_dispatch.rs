c356e343d7f116033e2616076066ed1a86abfea1
bear <boundless.forest@outlook.com>
2022-07-01 16:35:31 +0800

    Fix

--- runtime/pangolin-parachain/src/pallets/bridge_dispatch.rs
+++ runtime/pangolin-parachain/src/pallets/bridge_dispatch.rs
@@ -2,18 +2,46 @@ pub use pallet_bridge_dispatch::Instance1 as WithPangolinDispatch;
 
 // --- paritytech ---
 use frame_support::traits::Everything;
+use sp_runtime::transaction_validity::{InvalidTransaction, TransactionValidityError};
 // --- darwinia-network ---
 use crate::*;
+use bp_message_dispatch::{CallValidate, IntoDispatchOrigin as IntoDispatchOriginT};
 use bp_messages::{LaneId, MessageNonce};
 use pallet_bridge_dispatch::Config;
+
+pub struct CallValidator;
+impl CallValidate<bp_pangolin_parachain::AccountId, Origin, Call> for CallValidator {
+	fn check_receiving_before_dispatch(
+		relayer_account: &bp_pangolin_parachain::AccountId,
+		call: &Call,
+	) -> Result<(), &'static str> {
+		Ok(())
+	}
+
+	fn call_validate(
+		relayer_account: &bp_pangolin_parachain::AccountId,
+		origin: &Origin,
+		call: &Call,
+	) -> Result<(), TransactionValidityError> {
+		Ok(())
+	}
+}
+
+pub struct IntoDispatchOrigin;
+impl IntoDispatchOriginT<bp_pangolin_parachain::AccountId, Call, Origin> for IntoDispatchOrigin {
+	fn into_dispatch_origin(id: &bp_pangolin_parachain::AccountId, _: &Call) -> Origin {
+		frame_system::RawOrigin::Signed(id.clone()).into()
+	}
+}
 
 impl Config<WithPangolinDispatch> for Runtime {
 	type AccountIdConverter = bp_pangolin_parachain::AccountIdConverter;
 	type BridgeMessageId = (LaneId, MessageNonce);
 	type Call = Call;
-	type CallFilter = Everything;
+	type CallValidator = CallValidator;
 	type EncodedCall = bm_pangolin::FromPangolinEncodedCall;
 	type Event = Event;
+	type IntoDispatchOrigin = IntoDispatchOrigin;
 	type SourceChainAccountId = bp_pangolin::AccountId;
 	type TargetChainAccountPublic = AccountPublic;
 	type TargetChainSignature = Signature;
