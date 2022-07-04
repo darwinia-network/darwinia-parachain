48cab2a8a87397817497f2de628b826c1f3e8401
Guantong <04637@163.com>
2022-07-04 14:38:56 +0800

    Companion for darwinia-messages-substrate#158

--- runtime/pangolin-parachain/src/pallets/bridge_dispatch.rs
+++ runtime/pangolin-parachain/src/pallets/bridge_dispatch.rs
@@ -1,30 +1,10 @@
 pub use pallet_bridge_dispatch::Instance1 as WithPangolinDispatch;
 
-// --- paritytech ---
-use sp_runtime::transaction_validity::TransactionValidityError;
 // --- darwinia-network ---
 use crate::*;
-use bp_message_dispatch::{CallValidate, IntoDispatchOrigin as IntoDispatchOriginT};
+use bp_message_dispatch::{Everything, IntoDispatchOrigin as IntoDispatchOriginT};
 use bp_messages::{LaneId, MessageNonce};
 use pallet_bridge_dispatch::Config;
-
-pub struct CallValidator;
-impl CallValidate<bp_pangolin_parachain::AccountId, Origin, Call> for CallValidator {
-	fn check_receiving_before_dispatch(
-		_relayer_account: &bp_pangolin_parachain::AccountId,
-		_call: &Call,
-	) -> Result<(), &'static str> {
-		Ok(())
-	}
-
-	fn call_validate(
-		_relayer_account: &bp_pangolin_parachain::AccountId,
-		_origin: &Origin,
-		_call: &Call,
-	) -> Result<(), TransactionValidityError> {
-		Ok(())
-	}
-}
 
 pub struct IntoDispatchOrigin;
 impl IntoDispatchOriginT<bp_pangolin_parachain::AccountId, Call, Origin> for IntoDispatchOrigin {
@@ -37,7 +17,7 @@ impl Config<WithPangolinDispatch> for Runtime {
 	type AccountIdConverter = bp_pangolin_parachain::AccountIdConverter;
 	type BridgeMessageId = (LaneId, MessageNonce);
 	type Call = Call;
-	type CallValidator = CallValidator;
+	type CallValidator = Everything;
 	type EncodedCall = bm_pangolin::FromPangolinEncodedCall;
 	type Event = Event;
 	type IntoDispatchOrigin = IntoDispatchOrigin;
