a9531a83b73d1c07d9a30df554f3034265ea5bc4
Guantong <04637@163.com>
2022-10-12 13:14:27 +0800

    To `polkadot-v0.9.27` (#150)
    * Anchor polkadot-v0.9.27
    
    * Companion for paritytech/cumulus#1436
    
    * Companion for paritytech/cumulus#1421
    
    * Companion for paritytech/cumulus#1350
    
    * Companion for paritytech/cumulus#1456
    
    * Use messages-substrate prepare-polkadot-v0.9.27 for debug
    
    * Companion for darwinia-network/darwinia-messages-substrate#169
    
    * Companion for darwinia-network/darwinia-messages-substrate#192
    
    * Companion for darwinia-network/darwinia-messages-substrate#186
    
    * Companion for darwinia-network/darwinia-messages-substrate#201
    
    * Missing part companion for darwinia-network/darwinia-messages-substrate#169
    
    * format
    
    * replace prepare-polkadot-v0.9.27 -> polkadot-v0.9.27
    
    * use `PANGOLIN_PANGOLIN_PARACHAIN_LANE` from `bridge_runtime_common` (#151)
    
    * format
    
    * Bump dependencies
    
    Co-authored-by: Xavier Lau <xavier@inv.cafe>

--- runtime/crab-parachain/src/pallets/fee_market.rs
+++ runtime/crab-parachain/src/pallets/fee_market.rs
@@ -11,7 +11,10 @@ use pallet_fee_market::{BalanceOf, Config, Slasher};
 
 pub struct FeeMarketSlasher;
 impl<T: Config<I>, I: 'static> Slasher<T, I> for FeeMarketSlasher {
-	fn slash(locked_collateral: BalanceOf<T, I>, timeout: T::BlockNumber) -> BalanceOf<T, I> {
+	fn cal_slash_amount(
+		locked_collateral: BalanceOf<T, I>,
+		timeout: T::BlockNumber,
+	) -> BalanceOf<T, I> {
 		let slash_each_block = 2 * COIN;
 		let slash_value = UniqueSaturatedInto::<Balance>::unique_saturated_into(timeout)
 			.saturating_mul(UniqueSaturatedInto::<Balance>::unique_saturated_into(slash_each_block))
@@ -29,16 +32,18 @@ frame_support::parameter_types! {
 	pub const CollateralPerOrder: Balance = 50 * COIN;
 	pub const Slot: BlockNumber = 600;
 
-	pub const AssignedRelayersRewardRatio: Permill = Permill::from_percent(60);
+	pub const DutyRelayersRewardRatio: Permill = Permill::from_percent(60);
 	pub const MessageRelayersRewardRatio: Permill = Permill::from_percent(80);
 	pub const ConfirmRelayersRewardRatio: Permill = Permill::from_percent(20);
+	pub const AssignedRelayerSlashRatio: Permill = Permill::from_percent(20);
 }
 
 impl Config<WithCrabFeeMarket> for Runtime {
-	type AssignedRelayersRewardRatio = AssignedRelayersRewardRatio;
+	type AssignedRelayerSlashRatio = AssignedRelayerSlashRatio;
 	type CollateralPerOrder = CollateralPerOrder;
 	type ConfirmRelayersRewardRatio = ConfirmRelayersRewardRatio;
 	type Currency = Ring;
+	type DutyRelayersRewardRatio = DutyRelayersRewardRatio;
 	type Event = Event;
 	type LockId = FeeMarketLockId;
 	type MessageRelayersRewardRatio = MessageRelayersRewardRatio;
