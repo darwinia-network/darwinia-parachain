9922119617586b04a28643fa60618cb4c2d53046
bear <boundless.forest@outlook.com>
2022-12-28 12:06:12 +0800

    Add solo-to-para pallet

--- runtime/crab-parachain/src/lib.rs
+++ runtime/crab-parachain/src/lib.rs
@@ -176,6 +176,7 @@ frame_support::construct_runtime! {
 		FromCrabIssuing: dp_common_runtime::helixbridge::{Pallet, Call, Storage, Event<T>} = 24,
 
 		MessageRouter: dp_common_runtime::message_router::{Pallet, Call, Storage, Event<T>} = 26,
+		SoloToPara: cumulus_pallet_solo_to_para::{Pallet, Call, Storage, Event} = 27,
 	}
 }
 
---
+++ runtime/pangolin-parachain/src/pallets/solo_to_para.rs
@@ -0,0 +1,6 @@
+// --- darwinia-network ---
+use crate::*;
+
+impl cumulus_pallet_solo_to_para::Config for Runtime {
+	type Event = Event;
+}
